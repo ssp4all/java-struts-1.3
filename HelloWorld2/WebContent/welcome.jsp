35e3563fc1f2118d8e6a46d0c39f9c03a01fa3fd
Suraj <2015suraj.pawar@ves.ac.in>
2020-05-23 12:21:43 -0400

    Init project

---
+++ HelloWorld2/WebContent/welcome.jsp
@@ -0,0 +1,5 @@
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="b"%>
+
+
+ <b:write name="helloForm" property="name" />
+' to Struts!
---
+++ HelloWorld2/src/info/java/tips/form/HelloForm.java
@@ -0,0 +1,24 @@
+package info.java.tips.form;
+
+import org.apache.struts.action.ActionForm;
+
+public class HelloForm extends ActionForm {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
+//	private static final long serialVersionUID = 1L;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
